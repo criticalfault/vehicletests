9ba2948169314716058b7cd45aaf308d7e829849
Dean Petty <dean.petty@gmail.com>
2023-12-12 20:49:03 -0600

    Adding lots of UX elements to try to break this thing up some

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import { useState } from 'react';
-
+import Accordion from 'react-bootstrap/Accordion';
 
 function App() {
 const [sensor, setSensor] = useState(0);
@@ -23,157 +23,177 @@ const [AutoNav, setAutoNav] = useState(0);
       <Container>
         <h1>Vehicle Test Helper</h1>
         <hr></hr>
-        <Row>
-          <h2>Vehicle Stats</h2>
-          <Col xs={12}>
-            <Form.Select aria-label="Vehicle Type" className="mb-3">
-              <option>Vehicle Type</option>
-              <option value="0">Car/Pickup</option>
-              <option value="3">Sports Car</option>
-              <option value="5">Motorcycle</option>
-              <option value="-3">Limousine/Van</option>
-              <option value="-5">Heavy Truck</option>
-              <option value="-3">Tracked Vehicle</option>
-              <option value="-7">Tractor Trailer</option>
-              <option value="5">Racing Boat</option>
-              <option value="-10">Yacht</option>
-              <option value="0">Small Airplane</option>
-              <option value="-5">Large Airplane</option>
-              <option value="20">Fighter Jet</option>
-              <option value="10">Low-Altitude-Vehicle/Thunderbird</option>
-              <option value="5">Helicopter</option>
-              <option value="2">Hovercraft</option>
-              <option value="-10">Lighter-than-Air/Zeppelin</option>
-              <option value="10">Ultralight Aircraft</option>
-              <option value="-10">Train/Monorail</option>
-            </Form.Select>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="Sensor">Sensor</InputGroup.Text>
-              <Form.Control
-                placeholder="Sensor"
-                aria-label="Sensor"
-                aria-describedby="Sensor"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="AutoNav">AutoNav</InputGroup.Text>
-              <Form.Control
-                placeholder="AutoNav"
-                aria-label="AutoNav"
-                aria-describedby="AutoNav"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="on_road_handling">On Road Handling</InputGroup.Text>
-              <Form.Control
-                placeholder="On Road Handling"
-                aria-label="On Road Handling"
-                aria-describedby="On Road Handling"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="off_road_handling">Off Road Handling</InputGroup.Text>
-              <Form.Control
-                placeholder="Off Road Handling"
-                aria-label="Off Road Handling"
-                aria-describedby="Off Road Handling"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="ECM">ECM</InputGroup.Text>
-              <Form.Control
-                placeholder="ECM"
-                aria-label="ECM"
-                aria-describedby="ECM"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="ECCM">ECCM</InputGroup.Text>
-              <Form.Control
-                placeholder="ECCM"
-                aria-label="ECCM"
-                aria-describedby="ECCM"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="Speed">Speed</InputGroup.Text>
-              <Form.Control
-                placeholder="Speed"
-                aria-label="Speed"
-                aria-describedby="Speed"
-              />
-            </InputGroup>
-          </Col>
-          <Col xs={6}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="MaxSpeed">Max Speed</InputGroup.Text>
-              <Form.Control
-                placeholder="Max Speed"
-                aria-label="Max Speed"
-                aria-describedby="Max Speed"
-              />
-            </InputGroup>
-          </Col>
-          <hr></hr>
-      </Row>
-      <Row>
-        <Col xs={6}>
-          <h3>Conditions</h3>
-          <div className="mb-3">
-            <Form.Check label="Unfamiliar vehicle"             value="1" name="unfamiliar_vehicle" type="checkbox" style={{"textAlign":"left"}}/>
-            <Form.Check label="Non-stressful situation"        value="-1" name="non_stressful_situation" type="checkbox" style={{"textAlign":"left"}}/>
-            <Form.Check label="Stressful situation"            value="1" name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Check label="Large vehicle of type"          value="2" name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Check label="Very large vehicle of type"     value="3" name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Check label="Weather bad"                    value="3" name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Check label="Weather terrible"               value="4" name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Check label="Action performed during combat" value="2" name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Check label="Using datajack but no VCR"      value="-1" name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Check label="Rigger in control"              value={VCR} name="" type="checkbox" style={{"textAlign":"left"}} />
-            <Form.Select aria-label="Terrian Select">
-              <option>Terrian Select</option>
-              <option value="-1">Open</option>
-              <option value="1">Restricted</option>
-              <option value="3">Tight</option>
-            </Form.Select>
-          </div>
-        </Col>
-        <Col xs={6}>
-          <h3>Rigger Stats</h3>
-          <InputGroup className="mb-3">
-            <InputGroup.Text id="Reaction">Reaction</InputGroup.Text>
-            <Form.Control
-              placeholder="Reaction"
-              aria-label="Reaction"
-              aria-describedby="Reaction"
-            />
-          </InputGroup>
-          <Form.Select aria-label="VCR Rating">
-              <option>VCR Rating</option>
-              <option value="0">None</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-          </Form.Select>
-        </Col>
-      </Row>
+        
+        <Accordion defaultActiveKey="0" alwaysOpen>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Vehicle Stats</Accordion.Header>
+            <Accordion.Body>
+              <Row>
+              <Col xs={12}>
+                <Form.Select aria-label="Vehicle Type" className="mb-3">
+                  <option>Vehicle Type</option>
+                  <option value="0">Car/Pickup</option>
+                  <option value="3">Sports Car</option>
+                  <option value="5">Motorcycle</option>
+                  <option value="-3">Limousine/Van</option>
+                  <option value="-5">Heavy Truck</option>
+                  <option value="-3">Tracked Vehicle</option>
+                  <option value="-7">Tractor Trailer</option>
+                  <option value="5">Racing Boat</option>
+                  <option value="-10">Yacht</option>
+                  <option value="0">Small Airplane</option>
+                  <option value="-5">Large Airplane</option>
+                  <option value="20">Fighter Jet</option>
+                  <option value="10">Low-Altitude-Vehicle/Thunderbird</option>
+                  <option value="5">Helicopter</option>
+                  <option value="2">Hovercraft</option>
+                  <option value="-10">Lighter-than-Air/Zeppelin</option>
+                  <option value="10">Ultralight Aircraft</option>
+                  <option value="-10">Train/Monorail</option>
+                </Form.Select>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="Sensor">Sensor</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Sensor"
+                    aria-label="Sensor"
+                    aria-describedby="Sensor"
+                  />
+                </InputGroup>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="AutoNav">AutoNav</InputGroup.Text>
+                  <Form.Control
+                    placeholder="AutoNav"
+                    aria-label="AutoNav"
+                    aria-describedby="AutoNav"
+                  />
+                </InputGroup>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="on_road_handling">On Road Handling</InputGroup.Text>
+                  <Form.Control
+                    placeholder="On Road Handling"
+                    aria-label="On Road Handling"
+                    aria-describedby="On Road Handling"
+                  />
+                </InputGroup>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="off_road_handling">Off Road Handling</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Off Road Handling"
+                    aria-label="Off Road Handling"
+                    aria-describedby="Off Road Handling"
+                  />
+                </InputGroup>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="ECM">ECM</InputGroup.Text>
+                  <Form.Control
+                    placeholder="ECM"
+                    aria-label="ECM"
+                    aria-describedby="ECM"
+                  />
+                </InputGroup>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="ECCM">ECCM</InputGroup.Text>
+                  <Form.Control
+                    placeholder="ECCM"
+                    aria-label="ECCM"
+                    aria-describedby="ECCM"
+                  />
+                </InputGroup>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="Speed">Speed</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Speed"
+                    aria-label="Speed"
+                    aria-describedby="Speed"
+                  />
+                </InputGroup>
+              </Col>
+              <Col xs={6}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="MaxSpeed">Max Speed</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Max Speed"
+                    aria-label="Max Speed"
+                    aria-describedby="Max Speed"
+                  />
+                </InputGroup>
+              </Col>
+              </Row>
+            </Accordion.Body>
+          </Accordion.Item>          
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Conditions / Rigger Stats</Accordion.Header>
+            <Accordion.Body>
+            <Row>
+              <Col xs={6}>
+                <h3>Conditions</h3>
+                <div className="mb-3">
+                  <Form.Check label="Unfamiliar vehicle"             value="1" name="unfamiliar_vehicle" type="checkbox" style={{"textAlign":"left"}}/>
+                  <Form.Check label="Non-stressful situation"        value="-1" name="non_stressful_situation" type="checkbox" style={{"textAlign":"left"}}/>
+                  <Form.Check label="Stressful situation"            value="1" name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Check label="Large vehicle of type"          value="2" name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Check label="Very large vehicle of type"     value="3" name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Check label="Weather bad"                    value="3" name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Check label="Weather terrible"               value="4" name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Check label="Action performed during combat" value="2" name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Check label="Using datajack but no VCR"      value="-1" name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Check label="Rigger in control"              value={VCR} name="" type="checkbox" style={{"textAlign":"left"}} />
+                  <Form.Select aria-label="Terrian Select">
+                    <option>Terrian Select</option>
+                    <option value="-1">Open</option>
+                    <option value="1">Restricted</option>
+                    <option value="3">Tight</option>
+                  </Form.Select>
+                </div>
+              </Col>
+              <Col xs={6}>
+                <h3>Rigger Stats</h3>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="Reaction">Reaction</InputGroup.Text>
+                  <Form.Control
+                    placeholder="Reaction"
+                    aria-label="Reaction"
+                    aria-describedby="Reaction"
+                  />
+                </InputGroup>
+                <Form.Select aria-label="VCR Rating">
+                    <option>VCR Rating</option>
+                    <option value="0">None</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </Form.Select>
+              </Col>
+            </Row>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <br></br><br></br>
       <Row>
         <Col>
           <Tabs defaultActiveKey="accel_break" id="uncontrolled-tab-example" className="mb-3">
+            <Tab eventKey="drive_test" title="Driving Test">
+              <p>Confer SR3 134.<br></br><br></br>The Driving Test is a complex action and is necessary whenever a character attempts something that isn't just part of
+               basic vehicle operation. See Skill Ratings and Meanings for examples of a character's general experience with a vehicle. To drive a car or accomplish
+                a standard landing or take-off maneuver, no test is necessary as long as the character has some ranks in the appropriate skill. Exceptional deeds like
+                high-speed maneuvers, stunts, vehicle operations under bad circumstances or combat (see below) require a Driving Test.<br></br><br></br>
+                To make a Driving Test, the player character rolls the most appropriate vehicle skill. The Target Number is the vehicle's Handling Rate. Rigger may add
+                dice from their Control Pool. In a non-combat situation, the Autonav Rating can be added as well.</p>
+            </Tab>
             <Tab eventKey="accel_break" title="Accelerating / Braking">
               <p>Confer SR3 141<br></br><br></br>
                 The driver makes a Driving Test using his vehicle skill against the vehicle's Handling Rating and applies modifiers from the Acceleration/Braking Target Modifier 
